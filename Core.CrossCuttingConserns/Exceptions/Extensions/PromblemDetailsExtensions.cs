0ccc6072251710046fbd4a268b72b38311f8fea0
Brackfled <oncellhsyn@outlook.com>
2023-12-18 22:33:47 +0300

    Add project files.

---
+++ Core.CrossCuttingConserns/Exceptions/Extensions/PromblemDetailsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConserns.Exceptions.Extensions
+{
+    public static class PromblemDetailsExtensions
+    {
+
+        public static string AsJson<TProblemDetail>(this TProblemDetail detail)
+            where TProblemDetail : ProblemDetails => JsonSerializer.Serialize(detail);
+
+    }
+}
---
+++ Core.CrossCuttingConserns/Exceptions/HttpProblemDetails/InternalServerProblemDetails.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConserns.Exceptions.HttpProblemDetails
+{
+    public class InternalServerProblemDetails : ProblemDetails
+    {
+
+        public InternalServerProblemDetails(string detail)
+        {
+            Title = "Internal Server Error";
+            Detail = "Internal Server Error";
+            Status = StatusCodes.Status500InternalServerError;
+            Type = "https://excample.com/probs/business";
+        }
+
+    }
+}
